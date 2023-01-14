49d9913358f2267307fa4ea832e516b417e16e9a
Frederick Emerson <frederick82004@gmail.com>
2023-01-14 12:42:49 +0800

    designed a sample page in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1> Dirac Spinor </h1>
+<p>In quantum field theory, the Dirac spinor is the spinor that describes all known fundamental particles that are fermions, with the possible exception of neutrinos. It appears in the plane-wave solution to the Dirac equation, and is a certain combination of two Weyl spinors, specifically, a bispinor that transforms "spinorially" under the action of the Lorentz group.
+
+    Dirac spinors are important and interesting in numerous ways. Foremost, they are important as they do describe all of the known fundamental particle fermions in nature; this includes the electron and the quarks. Algebraically they behave, in a certain sense, as the "square root" of a vector. This is not readily apparent from direct examination, but it has slowly become clear over the last 60 years that spinorial representations are fundamental to geometry. For example, effectively all Riemannian manifolds can have spinors and spin connections built upon them, via the Clifford algebra.[1] The Dirac spinor is specific to that of Minkowski spacetime and Lorentz transformations; the general case is quite similar.</p>
+<p class="right">This article is devoted to the Dirac spinor in the Dirac representation. This corresponds to a specific representation of the gamma matrices, and is best suited for demonstrating the positive and negative energy solutions of the Dirac equation. There are other representations, most notably the chiral representation, which is better suited for demonstrating the chiral symmetry of the solutions to the Dirac equation. The chiral spinors may be written as linear combinations of the Dirac spinors presented below; thus, nothing is lost or gained, other than a change in perspective with regards to the discrete symmetries of the solutions.</p>
+<style>
+    *{
+                
+        flex:1;
+        flex-direction: row;
+        justify-content: center;   
+    }
+    h1{
+        width: fit-content;
+        color: #9fffcb;
+        font-size: 80px;
+        font-weight: 800;
+        font-family: monospace;
+        text-shadow: 2px 2px 5px black;
+        text-align: center;
+        border-radius: 40px;
+        padding: 20px;
+        box-shadow: 3px 3px 4px #f2e9e4;
+        background-color: #e63946;
+    }
+    p{
+        padding-top: 10px;
+        color:white;
+        font-size: 20px;
+        font-family: sans-serif;
+        word-spacing: 5px;
+        line-height: 1.8;
+    }
+    p.right{
+        margin-left: 20%;
+        width: auto;
+    }
+</style>
